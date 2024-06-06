4207cff08aa5a73dcb074481941977e77abc2e1a
hansunwoo <gkstjsdn0011@naver.com>
2024-06-06 10:14:07 +0900

    2024-06-06

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,19 +2,24 @@ package org.example;
 
 
 
-import org.example.codingtest.Solution;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int [] a= new int[10];
-        int first = 0;
-        int last = 10;
-
+        Deque<Integer> a = new ArrayDeque<>();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        a.add(5);
+        a.add(6);
+        a.add(7);
+        Iterator<Integer> iterator = a.descendingIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
     }
 
 }

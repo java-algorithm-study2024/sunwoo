c8e32390eeaf521829bc8403b99c59064e5514e4
sunwoohan <gkstjsdn0011@naver.com>
2024-06-11 20:46:35 +0900

    0611

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,9 @@
 package org.example;
 
-
-
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String ans = "abcdefghijkln";
-        String t = ans.substring(0,4);
-        System.out.println(t);
 
     }
 }

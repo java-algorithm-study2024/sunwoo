b1672e8752022742413b71b3c5335023f836a4d4
sunwoohan <gkstjsdn0011@naver.com>
2024-06-12 18:59:59 +0900

    0612

--- src/main/java/org/example/codingtest/Solution.java
+++ src/main/java/org/example/codingtest/Solution.java
@@ -1,11 +1,9 @@
 package org.example.codingtest;
 
 class Solution {
-    public int solution(String num_str) {
-        int answer = 0;
-        for(int i =0;i<num_str.length();i++){
-            answer += Integer.parseInt(String.valueOf(num_str.charAt(i)));
-        }
+    public int[] solution(int[] prices) {
+        int[] answer = {};
+
         return answer;
     }
 }

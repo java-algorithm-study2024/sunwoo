320588a9ed9b86a595442695d37558f38c565ff6
hansunwoo <gkstjsdn0011@naver.com>
2024-06-13 17:24:04 +0900

    0613

--- src/main/java/org/example/codingtest/Solution.java
+++ src/main/java/org/example/codingtest/Solution.java
@@ -1,17 +1,8 @@
 package org.example.codingtest;
 
 class Solution {
-    public int solution(String ineq, String eq, int n, int m) {
+    public int solution(int a, int b, boolean flag) {
         int answer = 0;
-        boolean ans = false;
-        if(ineq == "<") {
-            if(eq == "=") ans = n<=m;
-            else ans = n<m;
-        }else{
-            if(eq == "=") ans = n>=m;
-            else ans = n>m;
-        }
-        if (ans) return 1;
-        return 0;
+        return answer;
     }
 }

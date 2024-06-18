fe11c090be11b29780e0ae1f352caf0ab69edee9
sunwoohan <gkstjsdn0011@naver.com>
2024-06-18 20:38:17 +0900

    0618

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,37 +9,11 @@ import java.util.*;
 
 public class Main {
     static class Solution{
-        public int[] solution(int[] prices) {
-            int[] answer = new int[prices.length];
-            Queue<Integer> a = new LinkedList<>();
-            for(int i : prices){
-                a.add(i);
-            }
-            int check  = 0;
-            while(!a.isEmpty()){
-                int num = a.poll();
-                if (a.isEmpty()){
-                    answer[check] = 0;
-                    break;
-                }
-                int count = 0;
-                for(int i : a){
-                    count += 1;
-                    if(i < num){
-                        answer[check] = count;
-                        break;
-                    }
-                }
-                answer[check] = count;
-                check += 1;
-            }
-            return answer;
-        }
+
     }
     public static void main(String[] args) throws IOException {
-        Solution s = new Solution();
-        int[] arr = {1,2,3,2,3};
-        s.solution(arr);
+
+
 
     }
 }

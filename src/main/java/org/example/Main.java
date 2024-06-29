ac844e01ee2937a78343b3f098c0ce5cb6320e7a
sunwoohan <gkstjsdn0011@naver.com>
2024-06-29 22:52:08 +0900

    0629

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,50 +3,63 @@ package org.example;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
 
 public class Main {
-    static List<Integer> [] listArray;
-    static boolean[] visit;
-
+    static String [] visit;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String node = br.readLine();
-        String edge = br.readLine();
-        int nodeCount = Integer.parseInt(node)+1;
-        int edgeCount = Integer.parseInt(edge);
-        String str;
-        String [] st;
-        int a,b;
-        visit = new boolean[nodeCount + 1];
-        listArray = new ArrayList[nodeCount];
-        for (int i = 0; i < nodeCount; i++) {
-            listArray[i] = new ArrayList<>();
+        String str = br.readLine();
+        StringTokenizer stringTokenizer = new StringTokenizer(str);
+        int sb = Integer.parseInt(stringTokenizer.nextToken());
+        int target = Integer.parseInt(stringTokenizer.nextToken());
+        visit = new String[target + 1];
+        Queue<Integer> que = new ArrayDeque<>();
+        que.add(sb);
+        for (int i = 0; i < target+1; i++) {
+            visit[i] = "";
         }
-        for (int i = 0; i < edgeCount; i++) {
-            str = br.readLine();
-            st = str.split(" ");
-            a = Integer.parseInt(st[0]);
-            b = Integer.parseInt(st[1]);
-            listArray[a].add(b);
-            listArray[b].add(a);
-
-        }
-        visit[1] = true;
-        int count =0;
-        Stack<Integer> stk = new Stack<>();
-        stk.add(1);
-        while (!stk.isEmpty()) {
-            int top = stk.pop();
-            for(int i : listArray[top]){
-                if (!visit[i]){
-                    visit[i] = true;
-                    stk.add(i);
-                    count +=1;
-                }
+        visit[sb] = "a";
+        while (visit[target] == "") {
+            int temp = que.poll();
+            int plus = temp +1;
+            int minus = temp -1;
+            int mul = temp * 2;
+            if (plus<=target && visit[plus] == "") {
+                String str_temp = visit[temp] + '+';
+                visit[plus] = str_temp;
+                que.add(plus);
+            }
+            if (minus >= 0 && visit[minus] == "") {
+                String str_temp = visit[minus] + '-';
+                visit[minus] = str_temp;
+                que.add(minus);
+            }
+            if (mul<=target && visit[mul] == "") {
+                String str_temp = visit[temp] + '*';
+                visit[mul] = str_temp;
+                que.add(mul);
             }
         }
-        System.out.println(count);
+        System.out.println(visit[target].length()-1);
+        if(visit[target] == "a") ;
+        else{
+        for (int i = 0; i < visit[target].length(); i++) {
+            char temp = visit[target].charAt(i);
+            if (temp == '+') {
+                sb +=1;
+                System.out.print(" " +sb);
+            }else if(temp=='-'){
+                sb -=1;
+                System.out.print(" " + sb);
+            }else if(temp=='*'){
+                sb *= 2;
+                System.out.print(" " + sb);
+            }
+        }}
         br.close();
     }
 }

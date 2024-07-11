f27b83abdaf1a31939f97d97615bc9eeb6fd86a5
sunwoohan <gkstjsdn0011@naver.com>
2024-07-11 16:54:25 +0900

    0711

--- src/main/java/org/example/codingtest/Solution.java
+++ src/main/java/org/example/codingtest/Solution.java
@@ -1,15 +1,57 @@
 package org.example.codingtest;
 
-class Solution {
-    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        int answer = 0;
-        boolean[][] map = new boolean[51][51];
-        for (int[] i : rectangle) {
-            for (int j = i[0]; j < i[1] + 1; j++) {
+import java.util.*;
 
+class Solution {
+    public int[] solution(String[] info, String[] query) {
+        HashMap<Integer, ArrayList<Integer>> hash = new HashMap<>();
+        Set<Integer> temp;
+        int[] answer = new int[info.length];
+        for (String str : info) {
+            int sum = view(str);
+            int count = sum % 1000;
+            sum = sum /1000;
+            if(hash.containsKey(sum)){
+                hash.get(sum).add(count);
+            }else{
+                hash.put(sum, new ArrayList<>());
+                hash.get(sum).add(count);
             }
         }
+        System.out.println(hash);
+        int i =0;
 
         return answer;
+    }
+
+
+    private int view(String str) {
+        int sum = 0;
+        String [] temp = str.split(" ");
+        System.out.println(temp[0] + " " + temp[1] + " " + temp[2] + " " + temp[3]);
+        if(Objects.equals(temp[0], "java")) {
+            sum += 1000000;
+        }else if(Objects.equals(temp[0], "python")){
+            sum += 2000000;
+        }else{
+            sum += 3000000;
+        }
+        if(Objects.equals(temp[1], "backend")){
+            sum += 100000;
+        }else{
+            sum +=200000;
+        }
+        if(Objects.equals(temp[2], "junior")){
+            sum += 10000;
+        }else{
+            sum +=20000;
+        }
+        if(Objects.equals(temp[3], "pizza")){
+            sum += 1000;
+        }else{
+            sum += 2000;
+        }
+        sum += Integer.parseInt(temp[4]);
+        return sum;
     }
 }

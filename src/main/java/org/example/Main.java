4fb84be0ecd6fc15c609bcdc536d191cddd7ff0f
hamster0410 <gkstjsdn0011@naver.com>
2024-07-17 23:56:48 +0900

    0717

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,31 +9,27 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String [] temp = br.readLine().split(" ");
-        int N = Integer.parseInt(temp[0]);
-        int D = Integer.parseInt(temp[1]);
-        int K = Integer.parseInt(temp[2]);
-        int C = Integer.parseInt(temp[3]);
-        int[] sushi = new int[N];
-        int[] list = new int[N + K - 1];
-        for (int i = 0; i < N; i++) {
-            sushi[i] = Integer.parseInt(br.readLine());
+        int dish_num = Integer.parseInt(temp[0]);
+        int sushi_variety = Integer.parseInt(temp[1]);
+        int sequence_dish = Integer.parseInt(temp[2]);
+        int coupon = Integer.parseInt(temp[3]);
+        int []sushi_table = new int[dish_num + sequence_dish];
+        for (int i = 0; i < dish_num; i++) {
+            sushi_table[i] = Integer.parseInt(br.readLine());
         }
-        for (int i = 0; i < K - 1; i++) {
-            list[N++] = list[i];
+        for (int i = 0; i < sequence_dish; i++) {
+            sushi_table[dish_num + i] = sushi_table[i];
         }
-        int[] eaten = new int[D + 1];
-        eaten[C] +=1;
-        int max =1;
-        int start = 0;
-        for (int i = start; i < K; i++) {
-            if(eaten[list[i]] == 0){
-                max ++;
+        int max = 0;
+
+        for (int i = 0; i < sushi_table.length; i++) {
+            Set<Integer> sushi_set = new HashSet<>();
+            for (int j = i; j < i + sequence_dish; j++) {
+                sushi_set.add(sushi_table[j]);
             }
-            eaten[list[i]] +=1;
+            max = Math.max(max, sushi_set.size());
         }
-        start = 0;
-        int end = K;
-        int result= max;
-        for(int i = end;)
+        System.out.println(max);
+
     }
 }

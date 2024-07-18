18000b8f0f13bf9036b7603aaad22f58365c3a75
sunwoohan <gkstjsdn0011@naver.com>
2024-07-18 18:15:34 +0900

    0718

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,47 +2,68 @@ package org.example;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
+    static int max = Integer.MIN_VALUE;
+    static Pair[] pairs;
+    static int Num;
+    static int answer = 0;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int arr[] = new int[N];
-        for (int i = 0; i < N; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
-        Arrays.sort(arr);
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                int snowMan1 = arr[i] + arr[j];
-                int start = 0;
-                int end = N - 1;
-                while (start < end) {
-                    if (start == i || start == j) {
-                        start++;
-                        continue;
-                    }
-                    if (end == i || end == j) {
-                        end--;
-                        continue;
-                    }
-                    int snowMan2 = arr[start] + arr[end];
-                    min = Math.min(min, Math.abs(snowMan1 - snowMan2));
-                    if (snowMan1 > snowMan2)
-                        start++;
-                    else if (snowMan1 < snowMan2)
-                        end--;
-                    else {
-                        System.out.println(0);
-                        return;
-                    }
-                }
-            }
+        Num = Integer.parseInt(br.readLine());
+        pairs = new Pair[Num];
+        for (int i = 0; i < Num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int endure = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            pairs[i] = new Pair(endure, weight);
         }
-        System.out.println(min);
+        crush(0,0);
+        System.out.println(max);
+    }
+
+    private static void crush(int wp, int cnt) {
+        // 마지막 계란까지 다 들어보면 종료
+        if (wp == Num) {
+            max = Math.max(max,cnt);
+            return;
+        }
+        //손으로 든 계란이 이미 깨졌거나 모든 계란이 다 깨져있다면
+        if(pairs[wp].endure <= 0 || cnt == Num -1){
+            crush(wp + 1,cnt);
+            return;
+        }
+        int nCnt = cnt;
+        for (int i = 0; i < Num; i++) {
+            if(i == wp) continue;
+            if(pairs[i].endure <= 0 ) continue;
+            pairs[wp].endure -= pairs[i].weight;
+            pairs[i].endure -= pairs[wp].weight;
+            if(pairs[wp].endure <= 0){
+                cnt ++;
+            }
+            if(pairs[i].endure <= 0 ){
+                cnt ++;
+            }
+            crush(wp +1, cnt);
+            pairs[i].endure += pairs[wp].weight;
+            pairs[wp].endure += pairs[i].weight;
+            cnt = nCnt;
+        }
+
+
+
+    }
+
+
+}
+class Pair{
+    int endure;
+    int weight;
+
+    Pair(int endure, int weight) {
+        this.endure = endure;
+        this.weight = weight;
     }
 }

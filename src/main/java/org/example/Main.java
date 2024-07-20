189aa04330eeca6bcba226710e33a22eb786f1e5
hamster0410 <gkstjsdn0011@naver.com>
2024-07-20 10:46:01 +0900

    0720

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,34 +2,73 @@ package org.example;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
+    static int max = 0;
+    static int L;
+    static int C;
+    static String mogroup = "aeiou";
+    static int mo;
+    static int ja;
+    static String [] words;
+    static Stack<String> password = new Stack<>();
+    static ArrayList<String> answer;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String [] temp = br.readLine().split(" ");
-        int dish_num = Integer.parseInt(temp[0]);
-        int sushi_variety = Integer.parseInt(temp[1]);
-        int sequence_dish = Integer.parseInt(temp[2]);
-        int coupon = Integer.parseInt(temp[3]);
-        int []sushi_table = new int[dish_num + sequence_dish];
-        for (int i = 0; i < dish_num; i++) {
-            sushi_table[i] = Integer.parseInt(br.readLine());
-        }
-        for (int i = 0; i < sequence_dish; i++) {
-            sushi_table[dish_num + i] = sushi_table[i];
-        }
-        int max = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < sushi_table.length; i++) {
-            Set<Integer> sushi_set = new HashSet<>();
-            for (int j = i; j < i + sequence_dish; j++) {
-                sushi_set.add(sushi_table[j]);
-            }
-            max = Math.max(max, sushi_set.size());
+        answer = new ArrayList<>();
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        words = br.readLine().split(" ");
+        Arrays.sort(words);
+        func(0);
+        for (String str : answer) {
+            System.out.println(str);
         }
-        System.out.println(max);
 
     }
+
+    private static void func(int idx) {
+
+        if(password.size() == L){
+            if(mo>0 && ja>1){
+            answer.add(password_maker());
+            return;
+            }
+            return;
+        }
+
+        for (int i = 0; i < C; i++) {
+            if (!password.isEmpty()) {
+                if(password.peek().compareTo(words[i])>=0){
+                    continue;
+                }
+            }
+            if(mogroup.contains(words[i])){
+                mo++;
+            }else{
+                ja++;
+            }
+            password.add(words[i]);
+            func(i);
+            password.pop();
+            if(mogroup.contains(words[i])){
+                mo--;
+            }else{
+                ja--;
+            }
+        }
+
+    }
+
+    private static String password_maker() {
+        StringBuilder temp = new StringBuilder();
+        for (String str : password) {
+            temp.append(str);
+        }
+        return temp.toString();
+    }
+
 }
